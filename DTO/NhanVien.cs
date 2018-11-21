b08a32e6e905d90d5069b76ef98cfc6f85b30924
ungthanhlong <ungthanhlong251198@gmail.com>
2018-11-21 18:47:58 +0700

    up

--- DTO/NhanVien.cs
+++ DTO/NhanVien.cs
@@ -7,7 +7,7 @@ using System.Data;
 namespace DTO
 {
    public class NhanVien
-    {
+    {//aa
         public NhanVien(int id, string hoten, string ngaysinh, string gioitinh, string sdt, string diachi, int cmnd)
         {
             this.ID = id;
