71e724a2c989c6dfb1d819f57a24d6cf0f1a6620
ungthanhlong <ungthanhlong251198@gmail.com>
2018-11-21 18:40:53 +0700

    Up

--- PMQLThuVien/frmTable.Designer.cs
+++ PMQLThuVien/frmTable.Designer.cs
@@ -34,10 +34,13 @@
             this.btnTTDG = new System.Windows.Forms.Button();
             this.btnTTNV = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Location = new System.Drawing.Point(122, 1);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(862, 474);
@@ -93,6 +96,15 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -107,6 +119,7 @@
             this.Name = "frmTable";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Table";
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -119,5 +132,6 @@
         private System.Windows.Forms.Button btnTTDG;
         private System.Windows.Forms.Button btnTTNV;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
